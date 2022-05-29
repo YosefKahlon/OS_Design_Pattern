fad58eb583a62faef980d13ba2d37d15d9bf3d34
yosefkahlon <yosefkahlon53@gmail.com>
2022-05-30 00:27:09 +0300

    push

--- main1.c
+++ main1.c
@@ -4,10 +4,11 @@
 #include <stdio.h>
 #include "queue.h"
 #include "string.h"
-//#include "queue.c"
+#include "queue.c"
 #include <stdlib.h>
 #include <pthread.h>
 #include <unistd.h>
+#include <ctype.h>
 //#include "object_active.h"
 
 
@@ -38,50 +39,73 @@ Queue *third_q;
 //}
 
 void *encrypt(void *data) {
-    char str = (char *) data;
-    char temp[1024];
-
-    strcpy(temp, str);
+    char str[strlen(data)];// = (char *) data;
+    strcpy(str, (char *) data);
 
 
-//    temp[0] = 's';
-//    char ch;
-//    size_t key = 1;
-//    for(int i = 0; str[i] != '\0'; ++i){
-//        ch = str[i];
-//        if(ch >= 'a' && ch <= 'z'){
-//            ch = ch + key;
-//            if(ch > 'z'){
-//                ch = ch - 'z' + 'a' - 1;
-//            }
-//            str[i] = ch;
-//        }
-//        else if(ch >= 'A' && ch <= 'Z'){
-//            ch = ch + key;
-//            if(ch > 'Z'){
-//                ch = ch - 'Z' + 'A' - 1;
-//            }
-//            str[i] = ch;
-//        }
-//    }
-    return temp;
+    char ch;
+    int key = 1;
+    for (int i = 0; str[i] != '\0'; ++i) {
+        ch = str[i];
+        if (ch >= 'a' && ch <= 'z') {
+            ch = ch + key;
+            if (ch > 'z') {
+                ch = ch - 'z' + 'a' - 1;
+            }
+            str[i] = ch;
+        } else if (ch >= 'A' && ch <= 'Z') {
+            ch = ch + key;
+            if (ch > 'Z') {
+                ch = ch - 'Z' + 'A' - 1;
+            }
+            str[i] = ch;
+        }
+    }
+
+    void *ans = str;
+
+    return ans;
 }
 
-//void * pass_to_second(void * data) {
-//    enQ(&second_q, data);
-//}
-//
-//void * upper_lower(void * data) {
-//
-//}
-//
-//void * pass_to_third(void * data) {
-//
-//}
-//
-//void * send_to_client(void * data) {
-//
-//}
+void pass_to_second(void *data) {
+    enQ(&second_q, data);
+
+}
+
+void *upper_lower(void *data) {
+    char str[strlen(data)];
+    strcpy(str, (char *) data);
+    int i = 0;
+    while (str[i] != '\0') {
+        if (str[i] >= 'A' && str[i] <= 'Z') {
+            str[i] =   tolower(str[i]);
+        } else if (str[i] >= 'a' && str[i] <= 'z') {
+
+             str[i] = toupper(str[i]);
+        }
+        else {
+            printf("Bad input \n");
+            exit(0);
+
+        }
+        i++;
+    }
+
+    void* ans = str;
+    return ans;
+
+
+}
+
+void  pass_to_third(void * data) {
+    enQ(&third_q, (char *)data);
+
+}
+
+void * send_to_client(void * data) {
+    
+
+}
 //
 //void * validation(void * data) {
 //    printf("Passed the packet %s\n", (char *) data);
@@ -97,7 +121,7 @@ int main() {
 //    active_object *activeObject_UPPER_LOWER = newAO(second_q, upper_lower, pass_to_third);
 //    active_object *activeObject_return_to_client = newAO(third_q, send_to_client, validation);
 
-    printf("TEST: %s\n", (char *) encrypt("HI"));
+    printf("TEST: %s\n", (char *) upper_lower("HI"));
 
 
 //    pthread_t thread1, thread2;
